314eb11fafe05f5b2b05fe0bcb2e280820ccd73c
wangcong0621 <56657191+wangcong0621@users.noreply.github.com>
2019-10-18 18:03:10 +0800

    1

--- server/src/main/java/com/wc/hellospringbootvue/server/ServerApplication.java
+++ server/src/main/java/com/wc/hellospringbootvue/server/ServerApplication.java
@@ -1,7 +1,13 @@
 package com.wc.hellospringbootvue.server;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +16,11 @@ public class ServerApplication {
         SpringApplication.run(ServerApplication.class, args);
     }
 
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return factory -> {
+            ErrorPage error404Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/index.html");
+            factory.addErrorPages(error404Page);
+        };
+    }
 }
--- server/src/main/java/com/wc/hellospringbootvue/server/controller/HomeController.java
+++ server/src/main/java/com/wc/hellospringbootvue/server/controller/HomeController.java
@@ -1,12 +1,15 @@
 package com.wc.hellospringbootvue.server.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/getdata")
+    @ResponseBody
     public String getData() {
         return "Hello springboot vue!";
     }
